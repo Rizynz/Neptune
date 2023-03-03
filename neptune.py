c7c27e888bc9048c530e0b61fe494312217e98e3
Rizynz <97900601+Rizynz@users.noreply.github.com>
2023-03-02 19:54:35 -0600

    Update neptune.py

--- neptune.py
+++ neptune.py
@@ -1,3 +1,4 @@
 from colorama import Fore
 
-input(f'''{Fore.GREEN}are you dumb i said it is DEMO file only! you can buy the key from neptunekey.sellix.io and dm me on discord to get real file'''}
+input(f'''{Fore.GREEN}are you dumb i said it is DEMO file only! you can buy the key from neptunekey.sellix.io and dm me on discord to get real file''')
+
